3feb0b2f71cd000f548b2950ea1d7306115fa781
Edward Ofori <tetraed.llc@gmail.com>
2024-10-27 22:04:05 -0400

    Adding a seeds.sql file to populate the the databose

---
+++ seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO department (name) VALUES ('Engineering'), ('Sales'), ('Finance');
+INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 80000, 1), ('Sales Manager', 70000, 2), ('Accountant', 60000, 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('John', 'Doe', 1), ('Jane', 'Smith', 2);
