3430550cf5ca7ad6b5f4d10beb2f99333a3f41c8
Edward Ofori <tetraed.llc@gmail.com>
2024-10-28 19:17:37 -0400

    Refactoring

--- querries.js
+++ querries.js
@@ -1,22 +1,49 @@
 // queries.js
 import pool from './db';
 
-const getAllEmployees = async () => {
-    const res = await pool.query(`SELECT * FROM employee`);
-    return res.rows;
+const queries = {
+    viewDepartments: async () => {
+        const res = await pool.query(`SELECT * FROM department`);
+        return res.rows;
+    },
+
+    viewRoles: async () => {
+        const res = await pool.query(`SELECT * FROM role`); 
+        return res.rows;
+    }, 
+
+    viewEmployees: async () => {
+        const res = await pool.query(`SELECT * FROM employee`);
+        return res.rows;
+    },
+
+    addDepartment: async (name) => {
+        const res = await pool.query(
+            `INSERT INTO department (name) VALUES ($1) RETURNING *`,
+            [name]
+        );
+        return res.rows[0];
+    },
+
+    addRole: async (title, salary, departmentId) => {
+        const
+        res = await pool.query(
+            `INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3) RETURNING *`,
+            [title, salary, departmentId]
+        );
+        return res.rows[0];
+    },
+    
+    addEmployee: async (firstName, lastName, roleId, managerId) => {
+        const res = await pool.query(
+            `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *`,
+            [firstName, lastName, roleId, managerId]
+        );
+        return res.rows[0];
+    },
+    
+
+    // Define other query functions like updateEmployeeManager, viewEmployeesByManager, deleteEmployee, etc.
 };
 
-const addEmployee = async (firstName, lastName, roleId, managerId) => {
-    const res = await pool.query(
-        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *`,
-        [firstName, lastName, roleId, managerId]
-    );
-    return res.rows[0];
-};
-
-// Define other query functions like updateEmployeeManager, viewEmployeesByManager, deleteEmployee, etc.
-module.exports = {
-    getAllEmployees,
-    addEmployee,
-    // Export other functions here
-};
+export default queries;
